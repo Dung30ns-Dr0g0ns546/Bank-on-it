22320d52286f15be21aff6e663625dce168c86fd
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2025-03-18 14:23:48 +0000

    commit

--- checkingAccount.java
+++ checkingAccount.java
@@ -1,17 +1,27 @@
 class CheckingAccount implements HasMenu
-  ====================
-    double balance
-  ====================
+
+double balance
+
     CheckingAccount()
+
     CheckingAccount(double balance)
+
     main()
+
     string menu()
-    void start()   
+
+    void start()
+
     double getBalance()
+
     string getBalanceString()
+
     void setBalance(double balance)
+
     void checkBalance()
+
     private double getDouble()
+
     void makeDeposit()
-    void makeWithdrawal
-  ====================
+
+    void makeWithdrawal()
